0b632cc0aef0284eaf0ce83a7caa2295381779cc
padsley <padsley@gmail.com>
2025-03-06 18:32:56 -0600

    trying to add files

---
+++ MVME_freereadout_sorter.hpp
@@ -0,0 +1,11 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <string>
+#include <iostream>
+#include <TString.h>
+
+bool VerboseFlag = false;
+
+void MVME_freereadout_sorter(TString, double, std::string);
